fe600b8f670a6a4fda55235d0713dd86cec7ddec
Anthony Paikai <paikai.anthony@gmail.com>
2017-04-13 11:00:16 -0700

    Implement Legend and update Board to handle button

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import '../style/App.css';
 import CamperRow from './CamperRow';
+import Legend from './Legend';
 
 class Board extends Component {
 
 	constructor() {
 		super();
-		this.state = { recent: [], alltime: [] };
+		this.state = { recent: [], alltime: [] , isRecent: true};
+		this.handleClick = this.handleClick.bind(this);
 	}
 
 	componentDidMount() {
@@ -21,11 +23,23 @@ class Board extends Component {
 			.catch(err => console.log(err))
 	}
 
+	handleClick(){
+		this.setState({isRecent: !this.state.isRecent});
+		console.log("switching recent");
+	}
+
 	render() {
-		const camperRows = this.state.recent.map(item => <CamperRow key={item.lastUpdate} camper={item} />) 
+		const {isRecent} = this.state;
+		let camperRows = [];
+		if(this.state.isRecent){
+			 camperRows = this.state.recent.map(item => <CamperRow key={item.lastUpdate} camper={item} />) 
+		}else{
+			 camperRows = this.state.alltime.map(item => <CamperRow key={item.lastUpdate} camper={item} />)
+		}
 		return (
 			<div className="Board">
-				<h1>The Leaderboard{this.props.name}</h1>
+				<h1>The Leaderboard</h1>
+				<Legend isRecent={isRecent} handleClick={this.handleClick}/>
 				<ul>
 					{camperRows}
 				</ul>
--- src/Components/Legend.js
+++ src/Components/Legend.js
@@ -1,4 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../style/App.css';
+
+function Legend(props){
+	const {isRecent, handleClick} = props;
+	return( 
+		<div className="Legend">
+			<button className={isRecent && "active"}  onClick={() => handleClick() }>
+				
+				Sort by Past 30 days! 
+				
+			</button>
+			<button className={!isRecent && "active"} onClick={() => handleClick() }>
+	
+				Sort by All-time!
+			
+			</button>
+		</div>
+	);
+}
 
 export default Legend;
